5e08d5eb8233868610824dca7076f74db546238b
Vadim7380 <prikhodkovn@gmail.com>
2019-03-19 10:30:45 -0600

    js18/1

--- Code-Academy-Exercises/Introduction-To-JavaScript/Conditional-Statements/Comparison-Operators.js
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Conditional-Statements/Comparison-Operators.js
@@ -1,9 +1,6 @@
-let sale = true;
-
-sale = false;
-
-if (sale) {
-  console.log('Time to buy!');
+let hungerLevel = 7
+if (hungerLevel > 7) {
+  console.log('Time to eat!');
 } else {
-  console.log('Time to wait for a sale.')
+  console.log('We can eat later!');
 }
