e2f4cfe27bbba2bf01167021e667cbbb58c9fe42
Vadim7380 <prikhodkovn@gmail.com>
2019-03-19 12:03:36 -0600

    js21

--- Code-Academy-Exercises/Introduction-To-JavaScript/Conditional-Statements/Truthy-and-Falsy-Assignment.js
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Conditional-Statements/Truthy-and-Falsy-Assignment.js
@@ -1,16 +1,6 @@
-let wordCount = 56;
+let tool = 'marker';
 
-if (wordCount) {
-  console.log("Great! You've started your work!");
-} else {
-  console.log('Better get to work!');
-}
+// Use short circuit evaluation to assign  writingUtensil variable below:
+let writingUtensil = tool || 'pen'
 
-
-let favoritePhrase = '';
-
-if (favoritePhrase) {
-  console.log("This string doesn't seem to be empty.");
-} else {
-  console.log('This string is definitely empty.');
-}
+console.log(`The ${writingUtensil} is mightier than the sword.`);
