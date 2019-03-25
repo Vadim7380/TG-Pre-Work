e13547e09f3579c3ceeef94b19f210613d0dea87
Vadim7380 <prikhodkovn@gmail.com>
2019-03-24 22:05:58 -0600

    js113

--- Code-Academy-Exercises/Introduction-To-JavaScript/Intermediate-JavaScript-Modules/Import-as.js
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Intermediate-JavaScript-Modules/Import-as.js
@@ -1,9 +1,25 @@
-import Airplane from './airplane';
+import {availableAirplanes, flightRequirements, meetsStaffRequirements, meetsSpeedRangeRequirements} from './airplane';
 
 function displayFuelCapacity() {
-  Airplane.availableAirplanes.forEach(function(element){
-  console.log('Fuel Capacity of ' + element.name + ': ' + element.fuelCapacity);
+  availableAirplanes.forEach(function(element) {
+    console.log('Fuel Capacity of ' + element['name'] + ': ' + element['fuelCapacity']);
   });
 }
 
 displayFuelCapacity();
+
+function displayStaffStatus() {
+  availableAirplanes.forEach(function(element) {
+   console.log(element['name'] + ' meets staff requirements: ' + meetsStaffRequirements(element['availableStaff'], flightRequirements['requiredStaff']) );
+  });
+}
+
+displayStaffStatus();
+
+function displaySpeedRangeStatus() {
+  availableAirplanes.forEach(function(element) {
+   console.log(element['name'] + ' meets speed range requirements:' + meetsSpeedRangeRequirements(element['maxSpeed'], element['minSpeed'], flightRequirements['requiredSpeedRange']));
+  });
+}
+
+displaySpeedRangeStatus();
