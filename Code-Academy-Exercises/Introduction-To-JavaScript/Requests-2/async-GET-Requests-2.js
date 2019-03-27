dcbccf75b8b49710dff5dc4034dc9ee8a4104fc7
Vadim7380 <prikhodkovn@gmail.com>
2019-03-27 10:12:28 -0600

    js130

--- Code-Academy-Exercises/Introduction-To-JavaScript/Requests-2/async-GET-Requests-2.js
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Requests-2/async-GET-Requests-2.js
@@ -1,10 +1,12 @@
-fetch('https://api-to-call.com/endpoint').then(response => {
-  if (response.ok) {
-    return response.json();
+const getData = async () => {
+  try {
+    const response = await fetch('https://api-to-call.com/endpoint');
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      return jsonResponse;
+    }
+    throw new Error('Request failed!');
+  } catch (error) {
+    console.log(error);
   }
-  throw new Error('Request failed!');
-}, networkError => {
-  console.log(networkError.message);
-}).then(jsonResponse => {
-  return jsonResponse;
-});
+}
